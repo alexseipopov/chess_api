7a7f467824f13143733f43ef3973ddd5075b3c52
Garrosh Magrathean <garroshm@student.21-school.ru>
2023-02-25 09:34:30 +0300

    add Stage and Images

--- chess_api/database/model.py
+++ chess_api/database/model.py
@@ -21,3 +21,28 @@ class Child(db.Model):
     trainer = sa.Column(sa.String)
     parent = sa.Column(sa.String)
 
+class Tournament(db.Model):
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String)
+    start = sa.Column(sa.Date)
+    finish = sa.Column(sa.Date)
+    register = sa.Column(sa.Date)
+    place = sa.Column(sa.String)
+    address = sa.Column(sa.String)
+    map = sa.Column(sa.String)
+    stage = db.relationship("Stage", backref="tournament")
+    image = db.relationship("Images", backref="tournament")
+
+class Stage(db.Model):
+    stageId = sa.Column(sa.Integer, primary_key=True)
+    tournamentId = sa.Column(sa.Integer, sa.ForeignKey(Tournament.id))
+    name = sa.Column(sa.String)
+    start = sa.Column(sa.Date)
+    finish = sa.Column(sa.Date)
+
+class Images(db.Model):
+    imageId = sa.Column(sa.Integer, primary_key=True)
+    tournamentId = sa.Column(sa.Integer, sa.ForeignKey(Tournament.id))
+    src = sa.Column(sa.String)
+    type = sa.Column(sa.Integer)
+
